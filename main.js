3215325a95647a18b2910a5773d0215e3dfbd08d
Yuxiangbaozi <378928394@qq.com>
2019-04-04 09:46:21 +0800

    轮播

--- main.js
+++ main.js
@@ -77,12 +77,48 @@ function aaa(){
 /*********************轮播控制***************************/
 let n
 初始化()
-setInterval( () => {
-    cur($(`.box > img:nth-child(${(n+2)%3+1})`))
-    ready($(`.box > img:nth-child(${(n)%3+1})`))
-    end($(`.box > img:nth-child(${(n+1)%3+1})`))
-    n = n+1
-},3000)
+var timeID = setInterval( () => {
+    //cur($(`.box > img:nth-child(${(n+2)%3+1})`))
+    //ready($(`.box > img:nth-child(${(n)%3+1})`))
+    //end($(`.box > img:nth-child(${(n+1)%3+1})`))
+    end($(`.box > img:nth-child(${k(n)})`))
+        .one(`transitionend`, (k) =>{ready($(k.currentTarget))})
+    cur($(`.box > img:nth-child(${k(n+1)})`))
+    n++
+} , 3000)
+
+$('.yanshi > .box').on('mouseenter',function () {
+    window.clearInterval(timeID)
+})
+
+$('.yanshi > .box').on('mouseleave',function () {
+    timeID = setInterval( () => {
+        //cur($(`.box > img:nth-child(${(n+2)%3+1})`))
+        //ready($(`.box > img:nth-child(${(n)%3+1})`))
+        //end($(`.box > img:nth-child(${(n+1)%3+1})`))
+        end($(`.box > img:nth-child(${k(n)})`))
+            .one(`transitionend`, (k) =>{ready($(k.currentTarget))})
+        cur($(`.box > img:nth-child(${k(n+1)})`))
+        n++
+    } , 3000)
+})
+
+$(`.yanshi > button`).on('click',function(i){
+    n = i.currentTarget.id
+    end($(`.box > img.cur`))
+        .one(`transitionend`, (k) =>{ready($(k.currentTarget))})
+    cur($(`.box > img:nth-child(${k(n)})`))
+})
+
+function k(n){
+    if(n > 3){
+        n = n%3
+        if (n === 0) {
+            n = 3
+        }
+    }
+    return n
+}
 function cur(node){
     return node.removeClass('end ready').addClass('cur')
 }
@@ -96,12 +132,5 @@ function 初始化(){
     n = 1
     cur($(`.box > img:nth-child(1)`))
     ready($(`.box > img:nth-child(2)`))
-    end($(`.box > img:nth-child(3)`))
+    ready($(`.box > img:nth-child(3)`))
 }
-
-$(`.yanshi > button`).on('click',function(i){
-    n = i.currentTarget.id
-    cur($(`.box > img:nth-child(${(n+2)%3+1})`))
-    ready($(`.box > img:nth-child(${(n)%3+1})`))
-    end($(`.box > img:nth-child(${(n+1)%3+1})`))
-})
