4d40e001f1c01e057cf7012b1382013075a9d421
Yuxiangbaozi <378928394@qq.com>
2019-04-24 19:23:02 +0800

    添加留言板

--- main.js
+++ main.js
@@ -87,11 +87,11 @@ var timeID = setInterval( () => {
     n++
 } , 3000)
 
-$('.yanshi > .box').on('mouseenter',function () {
+$('.yanshi').on('mouseenter',function () {
     window.clearInterval(timeID)
 })
 
-$('.yanshi > .box').on('mouseleave',function () {
+$('.yanshi').on('mouseleave',function () {
     timeID = setInterval( () => {
         //cur($(`.box > img:nth-child(${(n+2)%3+1})`))
         //ready($(`.box > img:nth-child(${(n)%3+1})`))
@@ -103,7 +103,23 @@ $('.yanshi > .box').on('mouseleave',function () {
     } , 3000)
 })
 
-$(`.yanshi > button`).on('click',function(i){
+document.addEventListener('visibilitychange',function(){
+    if(document.hidden){
+        window.clearInterval(timeID)
+    }else{
+        timeID = setInterval( () => {
+            //cur($(`.box > img:nth-child(${(n+2)%3+1})`))
+            //ready($(`.box > img:nth-child(${(n)%3+1})`))
+            //end($(`.box > img:nth-child(${(n+1)%3+1})`))
+            end($(`.box > img:nth-child(${k(n)})`))
+                .one(`transitionend`, (k) =>{ready($(k.currentTarget))})
+            cur($(`.box > img:nth-child(${k(n+1)})`))
+            n++
+        } , 3000)
+    }
+})
+
+$(`.yanshi`).on('click','button',function(i){
     n = i.currentTarget.id
     end($(`.box > img.cur`))
         .one(`transitionend`, (k) =>{ready($(k.currentTarget))})
@@ -134,3 +150,74 @@ function 初始化(){
     ready($(`.box > img:nth-child(2)`))
     ready($(`.box > img:nth-child(3)`))
 }
+
+/*********************留言栏***************************/
+!function(){
+    var view = document.querySelector('section.massage')
+
+    var modle = {
+        find : function(){
+            var Massage = new AV.Query('Massage');
+            return Massage.find()
+        },
+        save : function(name,content){
+            var Massage = AV.Object.extend('Massage');
+            var massage = new Massage();
+            return massage.save({
+                'name': name,
+                'content': content
+            })
+        }
+    }
+
+    var control = {
+        view : null,
+        mas : null,
+        from : null,
+        init : function(view){
+            this.view = view
+            this.mas = view.querySelector('#massageList')
+            this.from = view.querySelector('.massage > #postmas')
+            this.initAV()
+            this.loadmassages()
+            this.bindEvents()
+        },
+        initAV : function(){
+            var APP_ID = 'ntxaCMSoh5fq2ckzp5LgpS33-gzGzoHsz'
+            var APP_KEY = 'rIEI9qygRxXr1BYmFiSGF2gI'
+            AV.init({ appId: APP_ID , appKey: APP_KEY });
+        },
+        loadmassages : function(){
+            modle.find().then( (massages) => {
+                let array = massages.map((massages) => massages.attributes)
+                array.forEach((item) => {
+                    let li = document.createElement('li')
+                    li.innerText = `${item.name}: ${item.content}`
+                    this.mas.appendChild(li)
+                });
+            }).then(function(todos) {
+                // 更新成功
+            }, function (error) {
+                // 异常处理
+            });
+        },
+        bindEvents : function(){
+            this.from.addEventListener('submit', (s) => {
+                s.preventDefault()
+                this.save.call()
+            })
+        },
+        save : function(){
+            var content = this.from.querySelector('input[name=content]').value
+            var name = this.from.querySelector('input[name=name]').value
+            modle.save(name,content).then((item) => {
+                let li = document.createElement('li')
+                li.innerText = `${item.attributes.name}: ${item.attributes.content}`
+                this.mas.appendChild(li)
+                from.querySelector('input[name=content]').value = ''
+            })
+        }
+    }
+
+    control.init(view)
+}.call()
